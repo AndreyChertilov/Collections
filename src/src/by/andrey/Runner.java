eb2722d51badc0670c38a7faff52a9cc89f40e9a
andreychertilov <andrey.4ertilov@gmail.com>
2017-02-26 22:49:23 +0300

    some update

--- src/src/by/andrey/Runner.java
+++ src/src/by/andrey/Runner.java
@@ -1,8 +1,15 @@
 package by.andrey;
 
-class Runner{
+import java.util.ArrayList;
+
+public class Runner {
 
     public static void main (String[] args) {
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Vania");
+        System.out.println(name);
+
 
 
     }
